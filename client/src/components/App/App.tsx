970449d6a809ec29e89e0784ffd04a1eae1fc177
Tania <taniyhamail@gmail.com>
2023-12-04 13:16:38 +0200

    fix: changed props name in IdeasAndTalentPage component

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,71 +1,65 @@
-import React, {useState, useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import {
-  useAppSelector,
-  useAppDispatch,
-} from "../../hooks/reduxToolkidHooks";
-import { changreMainPreloader } from "../SettingMenu/StateElementSlice";
-import { changeUserProfile } from "../../store/userSlice";
+import React, { useState, useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useAppSelector, useAppDispatch } from '../../hooks/reduxToolkidHooks'
+import { changreMainPreloader } from '../SettingMenu/StateElementSlice'
+import { changeUserProfile } from '../../store/userSlice'
 
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import MainPage from "../../pages/MainPage/MainPage";
-import AboutUs from "../../pages/AboutUs/AboutUs";
-import SingUpPage from "../../pages/SingUpPage/SingUpPage";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import IdeasAndTalent from "../../pages/IdeasAndTalent/IdeasAndTalentPage";
-import CreateIdea from "../../pages/CreateIdea/CreateIdea";
-import Preloader from "../Preloader/Preloader";
-import IdeasPopup from "../IdeasPopup/IdeasPopup";
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import MainPage from '../../pages/MainPage/MainPage'
+import AboutUs from '../../pages/AboutUs/AboutUs'
+import SingUpPage from '../../pages/SingUpPage/SingUpPage'
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage'
+import IdeasAndTalent from '../../pages/IdeasAndTalent/IdeasAndTalentPage'
+import CreateIdea from '../../pages/CreateIdea/CreateIdea'
+import Preloader from '../Preloader/Preloader'
+import IdeasPopup from '../IdeasPopup/IdeasPopup'
 
-import ServiceBanyak from "../../service/ServiceBanyak";
-import workWithCookies from "../../untils/workWithCookies";
+import ServiceBanyak from '../../service/ServiceBanyak'
+import workWithCookies from '../../untils/workWithCookies'
 
-import "./App.scss";
-import ProfilePage from "../../pages/ProfilePage/ProfilePage";
-import ChooseProfilePage from "../../pages/ChooseProfilePage/ChooseProfilePage";
-import ButtonChooseProfile from "../../atoms/ButtonChooseProfile/ButtonChooseProfile";
+import './App.scss'
+import ProfilePage from '../../pages/ProfilePage/ProfilePage'
+import ChooseProfilePage from '../../pages/ChooseProfilePage/ChooseProfilePage'
+import ButtonChooseProfile from '../../atoms/ButtonChooseProfile/ButtonChooseProfile'
 
 function App() {
-  const { mainPreloader } = useAppSelector(
-    (state) => state.stateElement
-  );
+  const { mainPreloader } = useAppSelector((state) => state.stateElement)
 
-  const location = useLocation();
+  const location = useLocation()
   // const popupLocation = location.search === '?login' || location.search === '?singup'
   const popupLocation =
-    location.search === "?login" || location.search === "?singup";
-  const { profileUser } = ServiceBanyak();
-  const { getCookies } = workWithCookies();
-  const [showPopup, setShowPopup] = useState(false);
+    location.search === '?login' || location.search === '?singup'
+  const { profileUser } = ServiceBanyak()
+  const { getCookies } = workWithCookies()
+  const [showPopup, setShowPopup] = useState(false)
 
-
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
-    const token = getCookies("sessiontokenid")
+    const token = getCookies('sessiontokenid')
     if (token !== null) {
       dispatch(changreMainPreloader(true))
       try {
-        profileUser(token, "GET")
+        profileUser(token, 'GET')
         dispatch(changreMainPreloader(false))
       } catch (e) {
         dispatch(changreMainPreloader(false))
-        console.error(e);
+        console.error(e)
       }
       // profileUser(token, "GET")
       //   .then((res) => )
       //   // .then(() => dispatch(changreMainPreloader(false)))
       //   .catch((e) => console.error(e))
     }
-    // eslint-disable-next-line 
-  }, []);
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <>
-      {popupLocation && <SingUpPage/>}
-      {mainPreloader && <Preloader/>}
-      {showPopup ? <IdeasPopup closeModal={setShowPopup}/> : null}
+      {popupLocation && <SingUpPage />}
+      {mainPreloader && <Preloader />}
+      {showPopup ? <IdeasPopup closeModal={setShowPopup} /> : null}
 
       <div className="app">
         <Header />
@@ -73,10 +67,13 @@ function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="aboutus" element={<AboutUs />} />
-            <Route path="ideas" element={<IdeasAndTalent type={true} />} />
-            <Route path="talents" element={<IdeasAndTalent type={false} />} />
-            <Route path="profile" element={<ProfilePage fc={setShowPopup}/>} />
-            <Route path="create-idea" element={<CreateIdea/>}/>
+            <Route path="ideas" element={<IdeasAndTalent ideaType={true} />} />
+            <Route
+              path="talents"
+              element={<IdeasAndTalent ideaType={false} />}
+            />
+            <Route path="profile" element={<ProfilePage fc={setShowPopup} />} />
+            <Route path="create-idea" element={<CreateIdea />} />
             <Route
               path="chose-profile"
               element={
@@ -103,7 +100,7 @@ function App() {
         <Footer />
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/IdeasAndTalent/IdeasAndTalentPage.tsx
+++ client/src/pages/IdeasAndTalent/IdeasAndTalentPage.tsx
@@ -1,70 +1,61 @@
-import { useEffect, useState } from "react";
-import ServiceBanyak from "../../service/ServiceBanyak";
-import { useAppDispatch } from "../../hooks/reduxToolkidHooks";
+import { useEffect, useState } from 'react'
+import ServiceBanyak from '../../service/ServiceBanyak'
+import { useAppDispatch } from '../../hooks/reduxToolkidHooks'
 
+import { Talent } from '../../types/types'
+import { changreMainPreloader } from '../../components/SettingMenu/StateElementSlice'
 
-import { Talent } from "../../types/types";
-import { changreMainPreloader } from "../../components/SettingMenu/StateElementSlice";
+import ButtonMoreLoading from '../../atoms/ButtonMoreLoading/ButtonMoreLoading'
+import Idea from '../../components/Idea/Idea'
+import Talant from '../../components/Talant/Talant'
 
-import ButtonMoreLoading from "../../atoms/ButtonMoreLoading/ButtonMoreLoading";
-import Idea from "../../components/Idea/Idea";
-import Talant from "../../components/Talant/Talant";
+import './IdeasAndTalent.scss'
 
-import "./IdeasAndTalent.scss";
+const IdeasAndTalent = ({ ideaType }: { ideaType: boolean }) => {
+  const [talents, setTalents] = useState<Talent[]>([])
+  const [ideas, setIdeas] = useState<Talent[]>([])
 
-const IdeasAndTalent = ({ type }: { type: boolean }) => {
-  const [talents, setTalents] = useState<Talent[]>([]);
-  const [ideas, setIdeas] = useState<Talent[]>([]);
-
-  const { getTalents, getIdeas } = ServiceBanyak();
+  const { getTalents, getIdeas } = ServiceBanyak()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    if (type) {
+    if (ideaType) {
       getIdeas()
-      .then((res) => res.json())
-      .then((res) => setIdeas(res))
-      .then(() => dispatch(changreMainPreloader(false)))
-      .catch(() => dispatch(changreMainPreloader(false)))
+        .then((res) => res.json())
+        .then((res) => setIdeas(res))
+        .then(() => dispatch(changreMainPreloader(false)))
+        .catch(() => dispatch(changreMainPreloader(false)))
     } else {
       getTalents()
-      .then((res) => res.json())
-      .then((res) => setTalents(res))
-      .then(() => dispatch(changreMainPreloader(false)))
-      .catch(() => dispatch(changreMainPreloader(false)))
-    } 
-   
-  }, [type]);
+        .then((res) => res.json())
+        .then((res) => setTalents(res))
+        .then(() => dispatch(changreMainPreloader(false)))
+        .catch(() => dispatch(changreMainPreloader(false)))
+    }
+  }, [ideaType])
 
-  
+  console.log('ideas', ideas)
   const renderItems = (type: boolean, data: Talent[]) => {
-    
-    
-
-    return data.map(item => {
-      return (
-        <>
-          {type ? <Idea myIdea={false} /> : <Talant dataUser={item}/>} 
-        </>
-      )
+    return data.map((item) => {
+      return <>{type ? <Idea myIdea={false} /> : <Talant dataUser={item} />}</>
     })
-  };
+  }
 
   return (
     <div className="ideaAndTalent">
       {/* {renderItems(type)} */}
       {/* {type ? <Idea myIdea={false}/> : <Talant />}
       {type ? <Idea myIdea={false}/> : <Talant />} */}
+      {ideaType ? <Idea myIdea={false} /> : <Talant dataUser={talents[0]} />}
+      {/* {talents ? renderItems(type, talents) : renderItems(type, ideas)} */}
 
-      {talents ? renderItems(type, talents) : renderItems(type, ideas)}
-
-      {type ? (
-        <ButtonMoreLoading text={"ідей"} />
+      {ideaType ? (
+        <ButtonMoreLoading text={'ідей'} />
       ) : (
-        <ButtonMoreLoading text={"талантів"} />
+        <ButtonMoreLoading text={'талантів'} />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default IdeasAndTalent;
+export default IdeasAndTalent
