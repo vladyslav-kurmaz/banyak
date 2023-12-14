46b99c7efc1e9ba1bd1a12a6ae48096aa7b0188f
Vladyslav Levkovich <levkovich.vlad.2004@gmail.com>
2023-12-14 15:57:23 +0200

    fix: site

--- server/src/users/models.py
+++ server/src/users/models.py
@@ -52,7 +52,7 @@ class Speciality(models.Model):
 
 class Stack(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=True, max_length=15)
+    name = models.CharField(unique=True, max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
