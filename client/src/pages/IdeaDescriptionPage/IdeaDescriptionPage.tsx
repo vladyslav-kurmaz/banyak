dd7d3d85a68d9cc11f03cef59a4b4ddf8522caa9
Tania <taniyhamail@gmail.com>
2023-12-26 17:04:02 +0200

    feat(ideasAndTalentsPages): added ideaDescription web version
    added  html markup and scss styles

--- client/src/pages/IdeaDescriptionPage/IdeaDescriptionPage.tsx
+++ client/src/pages/IdeaDescriptionPage/IdeaDescriptionPage.tsx
@@ -81,45 +81,15 @@ const IdeaDescriptionPage = () => {
           </div>
         </div>
       </div>
+      {/* // Web version */}
 
       <div className="idea-description">
-        <div className="idea-description__content-wraper">
-          <div className="idea-description__header">
-            <img
-              src={plugIcon}
-              alt="logo for idea"
-              className="idea-description__header-img"
-            />
-            <DisplayDateFromDB date={'2023-12-05T08:53:07.571657Z'} />
-            <ViewsIconAndQuantity viewsQuantity={10} />
-          </div>
-          <div className="idea-description__info">
-            <h1 className="idea-description__info-title">
-              Мобільний застосунок для домогосподарок
-            </h1>
-            <p className="idea-description__info-description">
-              Шукаю UI/UX дизайнера щоб розробити мобільний застосунок, для
-              домогосподарок, під ios платформу. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Architecto vel, quasi exercitationem
-              provident suscipit, maiores, molestias odio dolor officiis
-              blanditiis delectus dolores a expedita earum labore sapiente harum
-              accusamus! Modi, aliquam itaque amet dolor vitae doloribus! Nisi,
-              optio numquam voluptatem consequatur officia hic illum in! Dolore
-              aperiam voluptas praesentium nesciunt.
-            </p>
-          </div>
-          <div className="idea-description__stack">
-            <h4 className="idea-description__stack-title">
-              Потрібні технології:
-            </h4>
-            <div className="idea-description__stack-items-wraper">
-              <p className="idea-description__stack-item">+Figma</p>
-              <p className="idea-description__stack-item">+A/B testing</p>
-              <p className="idea-description__stack-item">+Adobe ILLustrator</p>
-              <p className="idea-description__stack-item">React Native</p>
-              <p className="idea-description__stack-item">Type Script</p>
-            </div>
-          </div>
+        <div className="idea-description__logo-specialities">
+          <img
+            src={plugIcon}
+            alt="logo for idea"
+            className="idea-description__img"
+          />
           <div className="idea-description__specialities">
             <h4 className="idea-description__specialities-title">
               Потрібні фахівці:
@@ -139,11 +109,51 @@ const IdeaDescriptionPage = () => {
               </p>
             </div>
           </div>
-          <div className="idea-description__btn-wraper">
-            <Link className="idea-description__btn-wraper-link-btn" to="/">
+        </div>
+        <div className="idea-description__info">
+          <div className="idea-description__info-title-description-wraper">
+            <h1 className="idea-description__info-title">
+              Мобільний застосунок для домогосподарок
+            </h1>
+            <p className="idea-description__info-description">
+              Шукаю UI/UX дизайнера щоб розробити мобільний застосунок, для
+              домогосподарок, під ios платформу. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Architecto vel, quasi exercitationem
+              provident suscipit, maiores, molestias odio dolor officiis
+              blanditiis delectus dolores a expedita earum labore sapiente harum
+              accusamus! Modi, aliquam itaque amet dolor vitae doloribus! Nisi,
+              optio numquam voluptatem consequatur officia hic illum in! Dolore
+              aperiam voluptas praesentium nesciunt.
+            </p>
+          </div>
+          <div className="idea-description__info-stack-btn-wraper">
+            <div className="idea-description__info-stack">
+              <h4 className="idea-description__info-stack-title">
+                Потрібні технології:
+              </h4>
+              <div className="idea-description__info-stack-items-wraper">
+                <p className="idea-description__info-stack-item">+Figma</p>
+                <p className="idea-description__info-stack-item">
+                  +A/B testing
+                </p>
+                <p className="idea-description__info-stack-item">
+                  +Adobe ILLustrator
+                </p>
+                <p className="idea-description__info-stack-item">
+                  React Native
+                </p>
+                <p className="idea-description__info-stack-item">Type Script</p>
+              </div>
+            </div>
+
+            <Link className="idea-description__info-link-btn" to="/">
               Відгукнутися
             </Link>
           </div>
+        </div>
+        <div className="idea-description__views-date-wraper">
+          <ViewsIconAndQuantity viewsQuantity={10} />
+          <DisplayDateFromDB date={'2023-12-05T08:53:07.571657Z'} />
         </div>
       </div>
     </>
