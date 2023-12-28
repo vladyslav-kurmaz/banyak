9bb2d7546a4cc2cd02c11cdc97ed797e95c3f557
Tania <taniyhamail@gmail.com>
2023-12-28 14:40:25 +0200

    feat(ideasAndTalents): added backButton
    added button which lead back to ideas list

--- client/src/pages/IdeaDescriptionPage/IdeaDescriptionPage.tsx
+++ client/src/pages/IdeaDescriptionPage/IdeaDescriptionPage.tsx
@@ -1,16 +1,18 @@
 import IdeaDescriptionPageTablet from '../../components/IdeaDescriptionPageTablet/IdeaDescriptionPageTablet'
 import IdeaDescriptionPageMobile from '../../components/IdeaDescriptionPageMobile/IdeaDescriptionPageMobile'
+import IdeaDescriptionPageDesktop from '../../components/IdeaDescriptionPageDesktop/IdeaDescriptionPageDesktop'
+import ButtonBack from '../../atoms/ButtonBack/ButtonBack'
 
 import './IdeaDescriptionPage.scss'
-import IdeaDescriptionPageDesktop from '../../components/IdeaDescriptionPageDesktop/IdeaDescriptionPageDesktop'
 
 const IdeaDescriptionPage = () => {
   return (
-    <>
+    <div className="idea-description-page-wraper">
+      <ButtonBack />
       <IdeaDescriptionPageMobile />
       <IdeaDescriptionPageTablet />
       <IdeaDescriptionPageDesktop />
-    </>
+    </div>
   )
 }
 
