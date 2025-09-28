4818c9e1477982857b12d61f6fa6c8ef6e98e677
Vladyslav Levkovych <levkovich.vlad.2004@gmail.com>
2025-09-28 16:08:34 +0300

    Update urls.py

--- server/banyak/urls.py
+++ server/banyak/urls.py
@@ -30,4 +30,8 @@ urlpatterns = [
 urlpatterns += doc_url
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
